4c1ee7ed68b4492c562ffd21a26f08b8324422a5
ShrineSigdel <123295702+ShrineSigdel@users.noreply.github.com>
2024-04-21 18:35:50 +0545

    feat: added local storage using getitem and setitem

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PersonalForm from './Components/PersonalForm';
 import EducationForm from './Components/EducationForm';
 import ExperienceForm from './Components/ExperienceForm';
 import CurriculumVitae from './Components/CurriculumVitae';
 
 export default function App() {
-  const [person, setPerson] = useState({});
-  const [education, setEducation] = useState([]);
-  const [experience, setExperience] = useState([]);
+  const [person, setPerson] = useState(() => {
+    const local = localStorage.getItem('PERSONS');
+    if (local === null) return {};
+    return JSON.parse(local);
+  });
+
+  const [education, setEducation] = useState(() => {
+    const local = localStorage.getItem('EDUCATION');
+    if (local === null) return [];
+    return JSON.parse(local);
+  });
+
+  const [experience, setExperience] = useState(() => {
+    const local = localStorage.getItem('EXPERIENCE');
+    if (local === null) return [];
+    return JSON.parse(local);
+  });
+
+  // Local Storage for persons, education and experience respectively
+  useEffect(() => {
+    localStorage.setItem('PERSONS', JSON.stringify(person));
+  }, [person]);
+
+  useEffect(() => {
+    localStorage.setItem('EDUCATION', JSON.stringify(education));
+  }, [education]);
+
+  useEffect(() => {
+    localStorage.setItem('EXPERIENCE', JSON.stringify(experience));
+  }, [experience]);
 
   function addPerson(fullname, dateofbirth, email, phone, location) {
     setPerson({
